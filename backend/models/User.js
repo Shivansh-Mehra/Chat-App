6d180d6a84d8fb5598ec67d614b364fe32614645
Shivansh Mehra <shivanshmehra293@gmail.com>
2025-01-04 03:59:25 +0530

    basic backend implement

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const userModel = new mongoose.model('User',userSchema);
+
+module.exports = userModel;
