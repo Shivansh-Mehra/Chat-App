7798182d2df511ed1d066b374e8e9e846f1fbcd2
Shivansh Mehra <shivanshmehra293@gmail.com>
2025-01-04 02:22:51 +0530

    added login and register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom"
 import Landing from "./Landing"
 import Chats from "./Chats"
+import Login from "./Login"
+import Register from "./Register"
 import './App.css'
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/view" element={<Chats />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   )
