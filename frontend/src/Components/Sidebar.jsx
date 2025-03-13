3c8eea438428f0b751817bb72832e3f130a19c99
Shivansh Mehra <shivanshmehra293@gmail.com>
2025-03-13 21:08:50 +0530

    again fix

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -102,7 +102,7 @@ export default function Sidebar() {
                   alt="profile pic"
                   className="border rounded-full w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12"
                 />
-                <div className="hidden lg:block">
+                <div className="block">
                   <p className="font-medium">{user.username}</p>
                   <p className="text-xs text-gray-500">{onlineUsers.includes(user._id) ? "Online" : "Offline"}</p>
                 </div>
